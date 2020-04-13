ff2b7c983b67ca099415ab226280b1b94ff1c082
Melanie Hallowell <melleehall@gmail.com>
2020-04-12 18:21:40 -0600

    Created SignupPathText component

---
+++ src/SignupPathText/SignupPathText.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './SignupPathText.css'
+
+
+export default class SignupPathText extends Component {
+    render() {
+        return (
+            <section class='signup-view'>
+                <section class='lightestgray'>
+                    <h2 class='signup-subhead'>Broadband Service Highlights</h2>
+                    <ul> 
+                        <li>High Speed Internet Connection</li>
+                        <li>High Touch Customer Service</li>
+                        <li>No Long-Term Contracts</li>
+                        <li>Easy Installation</li>
+                    </ul>
+                </section>
+            </section>
+        )
+    }
+}
